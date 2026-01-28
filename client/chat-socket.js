75fadd8759ee4b3a77265da142261c6ece5ba404
Ramz001 <rkenjaev1@gmail.com>
2026-01-28 19:26:23 +0500

    fix: correct formatting and structure in chat-socket.js and index.html; update context in docker-compose.yml

--- client/chat-socket.js
+++ client/chat-socket.js
@@ -1,25 +1,25 @@
 const socket = io("http://localhost:3000", {
   withCredentials: false,
-  transports: ['websocket', 'polling']
-})
+  transports: ["websocket", "polling"],
+});
 
-const message = document.getElementById('message');
-const messages = document.getElementById('messages');
+const message = document.getElementById("message");
+const messages = document.getElementById("messages");
 
 const handleSubmitNewMessage = () => {
-  socket.emit('message', { data: message.value })
-}
+  socket.emit("message", { data: message.value });
+};
 
-socket.on('message', ({ data }) => {
+socket.on("message", ({ data }) => {
   handleNewMessage(data);
-})
+});
 
 const handleNewMessage = (message) => {
   messages.appendChild(buildNewMessage(message));
-}
+};
 
 const buildNewMessage = (message) => {
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(message))
+  li.appendChild(document.createTextNode(message));
   return li;
-}
+};
